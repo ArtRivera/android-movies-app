f4733dfbf47da37d0237a1c87c8638a45480ddea
Arturo Rivera <art.riveram@gmail.com>
2025-07-25 14:54:53 -0600

    feat: MovieListFragment.kt, MovieDetailFragment.kt and ui part I done.

---
+++ app/src/main/java/com/artrivera/moviesapp/MovieDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.artrivera.moviesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/artrivera/moviesapp/MovieListFragment.kt
@@ -0,0 +1,12 @@
+package com.artrivera.moviesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class MovieListFragment : Fragment(R.layout.fragment_movie_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
