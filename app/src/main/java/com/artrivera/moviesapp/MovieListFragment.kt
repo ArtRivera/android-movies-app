cf3b48eb869ee3d1db7a494df4bd1b7e376f3264
Arturo Rivera <art.riveram@gmail.com>
2025-08-06 23:24:40 -0600

    feat: movie list and view binding active

--- app/src/main/java/com/artrivera/moviesapp/MovieListFragment.kt
+++ app/src/main/java/com/artrivera/moviesapp/MovieListFragment.kt
@@ -3,10 +3,14 @@ package com.artrivera.moviesapp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import com.artrivera.moviesapp.databinding.FragmentMovieListBinding
 
 class MovieListFragment : Fragment(R.layout.fragment_movie_list) {
 
+    private lateinit var binding: FragmentMovieListBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieListBinding.bind(view)
     }
 }
